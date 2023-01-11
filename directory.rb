1e96b6f59c65c4078725c855618134510811d0b0
Sidra Iqbal <siqbal@nmplondon.com>
2023-01-11 22:07:09 +0000

    Add comments to code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of Villians Academy"
 puts "-------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#finally, print the total number of students
 print "Overall we have"
+#print does not add new line characters vs. puts
 print 11
 print "great students"
