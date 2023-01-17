faae821460991ecee82c63d8868b0ff9472662bb
Sidra Iqbal <siqbal@nmplondon.com>
2023-01-17 22:00:55 +0000

    Exercise 5 updates with adjusting input with country & hobby

--- directory.rb
+++ directory.rb
@@ -1,20 +1,22 @@
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit the return twice"
-    # create an empty array
-    students =[]
-    # get the first name
-    name = gets.chomp
-    # while the name is NOT empty, repeat this code
-    while !name.empty? do
-        students << {name: name, cohort: :november}
+    students =[]     # create an empty array
+    name = gets.chomp # get the first name
+    while !name.empty? do # while name is NOT empty, repeat this code
+      puts "Please enter what country you are from: "
+      country = gets.chomp
+      puts "What is your most favourite hobby?"
+      hobby = gets.chomp
+      students << {name: name, cohort: :november, country: country, hobby: hobby}
         puts "Now we have #{students.count} students"
-        # get another name from the user
-        name = gets.chomp
+      puts "Enter another name: "  
+      name = gets.chomp
     end
     # return the array of students
     students
 end
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -23,8 +25,8 @@ end
 
 #Pass hash through of name and cohort
 def print(students)
-    students.each do |student|
-     puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    students.each_with_index do |student, index|
+     puts "#{index +1}. #{student[:name]}, #{student[:country]} (#{student[:cohort]} cohort) favourite hobby is #{student[:hobby]}"
     end
 end
 
@@ -59,16 +61,15 @@ print_footer(students)
 # 4) Rewrite the each() method that prints all students using 
 # while or until control flow methods.
 
-=begins
-def print_while(students)
+#def print_while(students)
   # iteration around the students array
-  puts "Displaying students using a WHILE"
-  index = 0
-  while index < students.count do
-    puts "#{index+1}. #{students[:name]} (#{students[:cohort]} cohort)"
-    index += 1
-  end
-  puts
-end
-=end
+ # puts "Displaying students using a WHILE"
+ # index = 0
+ # while index < students.count do
+ #   puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+ #   index += 1
+ # end
+ # puts
+#end
 
+# 5) Add in input for hobby and country
